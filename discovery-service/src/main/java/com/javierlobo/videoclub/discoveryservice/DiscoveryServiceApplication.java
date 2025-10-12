1d70f998ebbb36e630feaa83305d694e7df04b21
Javier Lobo <franciscojavier.lobo@gmail.com>
2025-10-13 00:15:33 +0200

    Commit inicial

---
+++ discovery-service/src/main/java/com/javierlobo/videoclub/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,12 @@
+package com.javierlobo.videoclub.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+}
